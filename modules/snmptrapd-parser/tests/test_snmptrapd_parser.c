2317982d49f3e1464284debf18a161ccacd6368c
László Várady <laszlo.varady@balabit.com>
2017-05-13 13:30:15 +0200

    snmptrapd-parser: add symbolic name parser tests

--- modules/snmptrapd-parser/tests/test_snmptrapd_parser.c
+++ modules/snmptrapd-parser/tests/test_snmptrapd_parser.c
@@ -93,11 +93,12 @@ TestSuite(snmptrapd_parser, .init = setup, .fini = teardown);
 
 Test(snmptrapd_parser, test_general_v2_message_with_oids)
 {
-  const gchar *input = "2017-05-10 12:46:14 web2-kukorica.syslog_ng.balabit [UDP: [127.0.0.1]:34257->[127.0.0.1]:162]:\n"
-                       "iso.3.6.1.2.1.1.3.0 = Timeticks: (875496867) 101 days, 7:56:08.67\t"
-                       "iso.3.6.1.6.3.1.1.4.1.0 = OID: iso.3.6.1.4.1.8072.2.3.0.1       "
-                       "iso.3.6.1.4.1.8072.2.3.2.1 = INTEGER: 60        \t "
-                       "iso.3.6.1.4.1.8072.2.1.3 = \"\"";
+  const gchar *input =
+    "2017-05-10 12:46:14 web2-kukorica.syslog_ng.balabit [UDP: [127.0.0.1]:34257->[127.0.0.1]:162]:\n"
+    "iso.3.6.1.2.1.1.3.0 = Timeticks: (875496867) 101 days, 7:56:08.67\t"
+    "iso.3.6.1.6.3.1.1.4.1.0 = OID: iso.3.6.1.4.1.8072.2.3.0.1       "
+    "iso.3.6.1.4.1.8072.2.3.2.1 = INTEGER: 60        \t "
+    "iso.3.6.1.4.1.8072.2.1.3 = \"\"";
 
   TestNameValue expected[] =
   {
@@ -128,6 +129,61 @@ Test(snmptrapd_parser, test_general_v1_message_with_oids)
     { ".snmp.subtype", ".17" },
     { ".snmp.uptime", "18:41:07.83" },
     { ".snmp.iso.3.6.1.4.1.8072.2.1.1", "123456" }
+  };
+
+  assert_log_message_name_values(input, expected, SIZE_OF_ARRAY(expected));
+}
+
+Test(snmptrapd_parser, test_v2_with_symbolic_names_and_various_types)
+{
+  const gchar *input =
+    "2017-05-13 12:17:32 localhost [UDP: [127.0.0.1]:52407->[127.0.0.1]:162]:  \n "
+    "mib-2.1.3.0 = Timeticks: (875496867) 101 days, 7:56:08.67 \t"
+    "snmpModules.1.1.4.1.0 = OID: netSnmpExampleHeartbeatNotification "
+    "netSnmpExampleHeartbeatRate = INTEGER: 60\t"
+    "netSnmpExampleString = STRING: \"string innerkey='innervalue'\"\t"
+    "org.2.2 = Gauge32: 22\t"
+    "org.1.1 = Counter32: 11123123   "
+    "org.5.3 = Hex-STRING: A0 BB CC DD EF \t"
+    "org.8.8 = NULL\t"
+    "dod.7 = IpAddress: 192.168.1.0\t  "
+    "org.5.9 = STRING: \"@\"";
+
+  TestNameValue expected[] =
+  {
+    { "HOST", "localhost" },
+    { ".snmp.transport_info", "UDP: [127.0.0.1]:52407->[127.0.0.1]:162" },
+    { ".snmp.snmpModules.1.1.4.1.0", "netSnmpExampleHeartbeatNotification" },
+    { ".snmp.netSnmpExampleHeartbeatRate", "60" },
+    { ".snmp.netSnmpExampleString", "string innerkey='innervalue'" },
+    { ".snmp.org.2.2", "22" },
+    { ".snmp.org.1.1", "11123123" },
+    { ".snmp.org.5.3", "A0 BB CC DD EF" },
+    { ".snmp.org.8.8", "NULL" },
+    { ".snmp.dod.7", "192.168.1.0" },
+    { ".snmp.org.5.9", "@" }
+  };
+
+  assert_log_message_name_values(input, expected, SIZE_OF_ARRAY(expected));
+}
+
+Test(snmptrapd_parser, test_v1_with_symbolic_names)
+{
+  const gchar *input =
+    "2017-05-13 12:18:30  localhost  [UDP: [127.0.0.1]:58143->[127.0.0.1]:162] : netSnmpExampleNotification \n"
+    "\t Warm Start Trap (1) Uptime:  27 days, 2:39:02.34\n "
+    "netSnmpExampleInteger = INTEGER: 123456 \t netSnmpExampleString = STRING: random string";
+
+  TestNameValue expected[] =
+  {
+    { "HOST", "localhost" },
+    { ".snmp.transport_info", "UDP: [127.0.0.1]:58143->[127.0.0.1]:162" },
+    { ".snmp.enterprise_oid", "netSnmpExampleNotification" },
+    { ".snmp.type", "Warm Start Trap" },
+    { ".snmp.subtype", "1" },
+    { ".snmp.uptime", "27 days, 2:39:02.34" },
+    { ".snmp.netSnmpExampleInteger", "123456" },
+    { ".snmp.netSnmpExampleString", "random string" }
   };
 
   assert_log_message_name_values(input, expected, SIZE_OF_ARRAY(expected));
