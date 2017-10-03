3e9561ff043588309f28dfb97c277e6d93451af7
László Várady <laszlo.varady@balabit.com>
2017-10-03 13:07:56 +0200

    !TEMP wec: copy-paste afsocket

---
+++ modules/wec/wec-dest.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2002-2012 Balabit
+ * Copyright (c) 1998-2012 Balázs Scheidler
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef WEC_DEST_H_INCLUDED
+#define WEC_DEST_H_INCLUDED
+
+#include "wec.h"
+#include "socket-options.h"
+#include "transport-mapper.h"
+#include "driver.h"
+#include "logwriter.h"
+
+#include <iv.h>
+
+typedef struct _AFSocketDestDriver AFSocketDestDriver;
+
+struct _AFSocketDestDriver
+{
+  LogDestDriver super;
+
+  gboolean
+    connections_kept_alive_accross_reloads:1;
+  gint fd;
+  LogWriter *writer;
+  LogWriterOptions writer_options;
+  LogProtoClientFactory *proto_factory;
+
+  GSockAddr *bind_addr;
+  GSockAddr *dest_addr;
+  gint time_reopen;
+  gboolean connection_initialized;
+  struct iv_fd connect_fd;
+  struct iv_timer reconnect_timer;
+  SocketOptions *socket_options;
+  TransportMapper *transport_mapper;
+
+  LogWriter *(*construct_writer)(AFSocketDestDriver *self);
+  gboolean (*setup_addresses)(AFSocketDestDriver *s);
+  const gchar *(*get_dest_name)(const AFSocketDestDriver *s);
+};
+
+static inline LogWriter *
+wec_dd_construct_writer(AFSocketDestDriver *self)
+{
+  return self->construct_writer(self);
+}
+
+static inline gboolean
+wec_dd_setup_addresses(AFSocketDestDriver *s)
+{
+  return s->setup_addresses(s);
+}
+
+static inline const gchar *
+wec_dd_get_dest_name(const AFSocketDestDriver *s)
+{
+  return s->get_dest_name(s);
+}
+
+LogWriter *wec_dd_construct_writer_method(AFSocketDestDriver *self);
+gboolean wec_dd_setup_addresses_method(AFSocketDestDriver *self);
+void wec_dd_set_keep_alive(LogDriver *self, gint enable);
+void wec_dd_init_instance(AFSocketDestDriver *self, SocketOptions *socket_options, TransportMapper *transport_mapper, GlobalConfig *cfg);
+LogTransport *wec_dd_construct_transport_method(AFSocketDestDriver *self, gint fd);
+
+gboolean wec_dd_init(LogPipe *s);
+void wec_dd_free(LogPipe *s);
+
+#endif
---
+++ modules/wec/wec-plugin.c
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2002-2012 Balabit
+ * Copyright (c) 1998-2012 Balázs Scheidler
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "wec-parser.h"
+#include "cfg-parser.h"
+#include "plugin.h"
+#include "tlscontext.h"
+#include "plugin-types.h"
+
+static Plugin wec_plugins[] =
+{
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "unix-stream",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "unix-stream",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "unix-dgram",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "unix-dgram",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "tcp",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "tcp",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "tcp6",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "tcp6",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "udp",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "udp",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "udp6",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "udp6",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "syslog",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "syslog",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "network",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_DESTINATION,
+    .name = "network",
+    .parser = &wec_parser,
+  },
+  {
+    .type = LL_CONTEXT_SOURCE,
+    .name = "systemd-syslog",
+    .parser = &wec_parser,
+  },
+};
+
+gboolean
+wec_module_init(GlobalConfig *cfg, CfgArgs *args)
+{
+  plugin_register(cfg, wec_plugins, G_N_ELEMENTS(wec_plugins));
+  return TRUE;
+}
+
+const ModuleInfo module_info =
+{
+  .canonical_name = "wec",
+  .version = SYSLOG_NG_VERSION,
+  .preference = 100,
+  .description = "The wec module provides socket based transports for syslog-ng, such as the udp(), tcp() and syslog() drivers. This module is compiled with SSL support.",
+  .core_revision = SYSLOG_NG_SOURCE_REVISION,
+  .plugins = wec_plugins,
+  .plugins_len = G_N_ELEMENTS(wec_plugins),
+};
