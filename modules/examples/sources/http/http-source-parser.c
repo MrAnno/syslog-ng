8fa62a0f2b18423f50fadd56d9e09cc25020b48c
László Várady <laszlo.varady@balabit.com>
2018-10-15 16:42:49 +0200

    examples: add HTTP source example
    Signed-off-by: László Várady <laszlo.varady@balabit.com>

---
+++ modules/examples/sources/http/http-source-parser.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018 Balabit
+ * Copyright (c) 2018 László Várady <laszlo.varady@balabit.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "driver.h"
+#include "cfg-parser.h"
+#include "http-source-grammar.h"
+
+extern int http_source_debug;
+
+int http_source_parse(CfgLexer *lexer, LogDriver **instance, gpointer arg);
+
+static CfgLexerKeyword http_source_keywords[] =
+{
+  { "http", KW_HTTP },
+  { NULL }
+};
+
+CfgParser http_source_parser =
+{
+#if ENABLE_DEBUG
+  .debug_flag = &http_source_debug,
+#endif
+  .name = "http_source",
+  .keywords = http_source_keywords,
+  .parse = (gint (*)(CfgLexer *, gpointer *, gpointer)) http_source_parse,
+  .cleanup = (void (*)(gpointer)) log_pipe_unref,
+};
+
+CFG_PARSER_IMPLEMENT_LEXER_BINDING(http_source_, LogDriver **)
+
---
+++ modules/examples/sources/http/http-source.c
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2018 Balabit
+ * Copyright (c) 2018 László Várady <laszlo.varady@balabit.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "http-source.h"
+#include "http/http-message.h"
+#include "http/http-message-internal.h"
+#include "http/source/transport-mapper-inet.h"
+#include "http/source/socket-options-inet.h"
+#include "logmsg/logmsg.h"
+
+#include <string.h>
+
+GQueue *
+_extract_messages(HTTPRequest *http_request, HTTPSourceConnection *connection)
+{
+  EHTTPSourceDriver *self = (EHTTPSourceDriver *) connection->owner;
+  MsgFormatOptions *parse_option = &self->super.reader_options.parse_options;
+
+  http_request_null_terminate_body(http_request);
+  const GByteArray *body = http_request_get_body(http_request);
+  if (!body)
+    return NULL;
+
+  GQueue *messages = g_queue_new();
+
+  gchar *state;
+  gchar *data = (gchar *) body->data;
+  gchar *line = strtok_r(data, "\n", &state);
+
+  while (line)
+    {
+      LogMessage *msg = log_msg_new(line, strlen(line), NULL, parse_option);
+      g_queue_push_tail(messages, msg);
+
+      line = strtok_r(NULL, "\n", &state);
+    }
+  return messages;
+}
+
+HTTPResponse *
+_create_response(HTTPRequest *http_request, HTTPSourceConnection *connection)
+{
+  HTTPResponse *http_response = http_response_new_empty();
+  http_response_set_http_version(http_response, 1, 1);
+  http_response_set_status_code(http_response, HTTP_OK);
+  http_response_add_header(http_response, "Content-Type", "text/html");
+  return http_response;
+}
+
+EHTTPSourceDriver *
+ehttp_sd_new(GlobalConfig *cfg)
+{
+  EHTTPSourceDriver *self = g_new0(EHTTPSourceDriver, 1);
+  http_sd_init_instance(&self->super, _socket_options_inet_new(), _transport_mapper_network_new(), cfg);
+
+  self->super.extract_log_messages = _extract_messages;
+  self->super.create_response = _create_response;
+
+  return self;
+}
