3fda4520a52ee9b2b976dc5ed42bd184a3993fdc
Laszlo Budai <laszlo.budai@balabit.com>
2019-04-12 15:07:05 +0200

    logsource: release dynamic window when log_source_free is called
    Signed-off-by: Laszlo Budai <laszlo.budai@balabit.com>

--- lib/logsource.c
+++ lib/logsource.c
@@ -550,12 +550,9 @@ log_source_free(LogPipe *s)
   log_pipe_detach_expr_node(&self->super);
   log_pipe_free_method(s);
 
-  //TODO: reclaim all dynamic window slots after the connection is closed and all acks have been received
-  // maybe here?
-  //dynamic_window_counter_release(self->dynamic_window.window_ctr, self->full_window_size - self->options->init_window_size);
-
   ack_tracker_free(self->ack_tracker);
   self->ack_tracker = NULL;
+  _release_dynamic_window(self);
 }
 
 void
